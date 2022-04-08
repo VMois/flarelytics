faa2c3f3445f9277d08e2a1412cfea92c5110e8e
VMois <vmois@protonmail.com>
2022-04-08 20:17:21 +0200

    api: allow access control from any origin

--- src/events.ts
+++ src/events.ts
@@ -2,10 +2,10 @@ import Storage, { RawEvent } from "./storage";
 
 
 const Events = async (request: any) => {
-    /*const headers = { 
+    const headers = { 
         "Access-Control-Allow-Origin": "*",
         "Content-type": "application/json",
-    };*/
+    };
 
     const event: RawEvent = await request.json();
     console.log("Received event: ", event);
@@ -15,10 +15,10 @@ const Events = async (request: any) => {
     
     try {
         await storage.saveEvent(event)
-        return new Response("")
+        return new Response("", { headers })
     } catch (error) {
         console.log(`Error. Couldn't store event: ${error}`)
-        return new Response("", { status: 500})
+        return new Response("", { status: 500 })
     }
 };
 
--- src/options.ts
+++ src/options.ts
@@ -1,15 +1,9 @@
 let headers: { [header: string]: string } = {
+    'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
     'Access-Control-Allow-Headers': "Origin, X-Requested-With, Content-Type, Accept",
 }
 
-const Options = () => {
-    if (ENVIRONMENT == "dev") {
-        headers['Access-Control-Allow-Origin'] = '*'
-        return new Response("", { headers })
-    } else {
-        return new Response("", { status: 500 })
-    }
-};
+const Options = () => new Response("", { headers });
 
 export default Options;
